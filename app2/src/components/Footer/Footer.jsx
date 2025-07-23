7105d419c0301b9d5d2281a10eb9e6e184855803
Aya Okasha <ayaskasha219@gmail.com>
2025-07-23 05:46:18 +0300

    footer edit

--- app2/src/components/Footer/Footer.jsx
+++ app2/src/components/Footer/Footer.jsx
@@ -5,57 +5,87 @@ import style from "./Footer.module.css";
 export default function Footer() {
   return (
     <>
-      <footer className="bg-dark text-white">
+      <footer className={style.footer}>
         <div className="container">
           <div className="row">
-            <div className="col-12 mt-3 text-center">
-              <ul className="list-unstyled list-inline">
+            <div className="col-12 text-center mb-3">
+              <ul
+                className={`list-unstyled list-inline ${style["social-icons"]}`}
+              >
                 <li className="list-inline-item mx-2">
-                  <Link to="/" className="text-white">
+                  <Link to="/">
                     <i className="fab fa-facebook-f fa-lg"></i>
                   </Link>
                 </li>
                 <li className="list-inline-item mx-2">
-                  <Link to="/" className="text-white">
+                  <Link to="/">
                     <i className="fab fa-twitter fa-lg"></i>
                   </Link>
                 </li>
                 <li className="list-inline-item mx-2">
-                  <Link to="/" className="text-white">
+                  <Link to="/">
                     <i className="fab fa-instagram fa-lg"></i>
                   </Link>
                 </li>
                 <li className="list-inline-item mx-2">
-                  <Link to="/" className="text-white">
+                  <Link to="/">
                     <i className="fab fa-google-plus fa-lg"></i>
                   </Link>
                 </li>
                 <li className="list-inline-item mx-2">
-                  <Link to="/" className="text-white">
+                  <Link to="/">
                     <i className="fa fa-envelope fa-lg"></i>
                   </Link>
                 </li>
               </ul>
             </div>
           </div>
-
-          <hr className="bg-white" />
-
-          <div className="row">
-            <div className="col-12 text-center">
+          <div className="row text-start text-md-start g-5">
+            <div className="col-md-4 col-xl-5 mb-4">
+              <h5>About Us</h5>
               <p>
-                <u>
-                  <Link
-                    to="/national-transaction"
-                    className="text-white text-decoration-underline"
-                  >
-                    National Transaction Corporation
-                  </Link>
-                </u>
-                is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned
-                subsidiary of U.S. Bancorp, Minneapolis, MN]
+                We are an award-winning creative agency, dedicated to the best
+                result in web design, promotion, business consulting, and
+                marketing.
               </p>
-              <p className="mb-0">© All rights reserved.</p>
+            </div>
+
+            <div className="col-md-4 mb-4">
+              <h5>Contacts</h5>
+              <p>
+                <strong>Address:</strong> 798 South Park Avenue, Jaipur, Raj
+              </p>
+              <p>
+                <strong>Email:</strong>{" "}
+                <a href="mailto:dkstudioin@gmail.com">dkstudioin@gmail.com</a>
+              </p>
+              <p>
+                <strong>Website:</strong>{" "}
+                <a href="https://karosearch.com">karosearch.com</a>
+              </p>
+            </div>
+
+            <div className="col-md-4 col-xl-3 mb-4">
+              <h5>Links</h5>
+              <ul className="list-unstyled">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <p className={style["bottom-text"]}>© All rights reserved.</p>
             </div>
           </div>
         </div>
